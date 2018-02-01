0ecace82f51939e872a4e44ce3d405e0095ecf18
Richard Blake <rblake@aafmaa.com>
2018-02-01 07:40:38 -0800

    Updated Jquery Plugin

--- plugins/jquery-updater/jquery-updater.php
+++ plugins/jquery-updater/jquery-updater.php
@@ -1,9 +1,10 @@
 <?php
-/*
+
+/**
  * Plugin Name: jQuery Updater
  * Plugin URI: http://www.ramoonus.nl/wordpress/jquery-updater/
  * Description: This plugin updates jQuery to the latest  stable version.
- * Version: 2.2.1
+ * Version: 3.3.1
  * Author: Ramoonus
  * Author URI: http://www.ramoonus.nl/
  * License: GPL3
@@ -11,34 +12,45 @@
  * Domain Path: /languages
  */
 
-/*
+/**
  * Replace jQuery with a newer version, load jQuery Migrate
- * @version 2.2.1
+ *
+ * @version 3.3.1
  * @since 1.0.0
  */
-function rw_jquery_updater() {
-
-	// jQuery
-	// Deregister core jQuery
-	wp_deregister_script('jquery');
-	// Register
-	wp_enqueue_script('jquery', plugins_url('/js/jquery-2.2.1.min.js', __FILE__), false, '2.2.1');
-
-	// jQuery Migrate
-	// Deregister core jQuery Migrate
-	wp_deregister_script('jquery-migrate');
-	// Register
-    wp_enqueue_script('jquery-migrate', plugins_url('/js/jquery-migrate-1.3.0.min.js', __FILE__), array('jquery'), '1.3.0'); // require jquery, as loaded above
+function rw_jquery_updater()
+{
+    
+    // jQuery
+    // Deregister core jQuery
+    wp_deregister_script('jquery');
+    // Register
+    wp_enqueue_script('jquery', plugins_url('/js/jquery-3.3.1.min.js', __FILE__), false, '3.3.1');
+    
+    // jQuery Migrate
+    // Deregister core jQuery Migrate
+    wp_deregister_script('jquery-migrate');
+    // Register
+    wp_enqueue_script('jquery-migrate', plugins_url('/js/jquery-migrate-3.0.0.min.js', __FILE__), array(
+        'jquery'
+    ), '3.0.0'); // require jquery, as loaded above
 }
-// Front-End
+
+/**
+ * Front-End
+ */
 add_action('wp_enqueue_scripts', 'rw_jquery_updater');
 
-/*
+/**
  * Load translation
+ *
  * @since 2.2.0
  * @version 1.0
-*/
-function rw__load_plugin_textdomain() {
-    load_plugin_textdomain( 'jquery-updater', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+ *         
+ */
+function rw__load_plugin_textdomain()
+{
+    load_plugin_textdomain('jquery-updater', FALSE, basename(dirname(__FILE__)) . '/languages/');
 }
-add_action( 'plugins_loaded', 'rw__load_plugin_textdomain' );
+
+add_action('plugins_loaded', 'rw__load_plugin_textdomain');
